617af890f8fed28277da4b1734e1ad86fee13c05
Nicola Hearn <Nicola.Hearn@EntainGroup.com>
2023-09-18 10:38:40 +0100

    Contains heading Ikea or Cheese test passes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("contains the header 'Ikea or Cheese'", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole("heading", {name: "Ikea or Cheese"})
   expect(linkElement).toBeInTheDocument();
 });
