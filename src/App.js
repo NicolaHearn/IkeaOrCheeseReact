890df1fbdef2a3b2be667490c7a10add53b6d761
Nicola Hearn <Nicola.Hearn@EntainGroup.com>
2023-09-27 16:43:52 +0100

    adds component to display the word to guess

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import LetsPlay from './components/LetsPlay';
 import './App.css';
 import GreetUser from './components/GreetUser';
+import DisplayWord from './components/DisplayWord';
 
 function App() {
   return (
     <div className="App">
+      <h1>Test</h1>
       <h1 className="app-header">Ikea or Cheese</h1>
       <LetsPlay />
       <GreetUser />
+      <DisplayWord word="cats"/>
     </div>
   );
 }
---
+++ src/components/DisplayWord.js
@@ -0,0 +1,9 @@
+function DisplayWord(props) {
+    return(
+       <div>
+        <h1>{props.word}</h1>
+       </div>
+    )
+}
+
+export default DisplayWord
