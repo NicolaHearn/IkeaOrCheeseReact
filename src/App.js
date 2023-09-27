c382e7ee82cba3e0da3f042d7e478e3db76252af
Nicola Hearn <Nicola.Hearn@EntainGroup.com>
2023-09-27 10:44:31 +0100

    adds greetUser component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import LetsPlay from './components/LetsPlay';
 import './App.css';
+import GreetUser from './components/GreetUser';
 
 function App() {
   return (
     <div className="App">
       <h1 className="app-header">Ikea or Cheese</h1>
       <LetsPlay />
+      <GreetUser />
     </div>
   );
 }
---
+++ src/components/GreetUser.js
@@ -0,0 +1,9 @@
+function greetUser() {
+    return(
+        <div>
+            <p>Hello player, type "i" for ikea or "c" for cheese</p>
+        </div>
+    )
+}
+
+export default greetUser
