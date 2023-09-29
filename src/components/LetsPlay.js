5d17d5717b1ec155659bbe48883d5d8e6914a432
Nicola Hearn <Nicola.Hearn@EntainGroup.com>
2023-09-29 14:07:35 +0100

    stores player name input

--- src/components/LetsPlay.js
+++ src/components/LetsPlay.js
@@ -1,10 +1,25 @@
+import React, { useState } from 'react';
+
 function LetsPlay() {
+   
+    const [enteredName, setEnteredName] = useState('');
+
+    const playerNameChangeHandler = (event) => {
+        setEnteredName(event.target.value)
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const player = enteredName;
+        console.log(player);
+    }
+
     return(
         <div data-testid="startGame">
             <h2>Let's play Ikea or Cheese</h2>
-            <form >
+            <form onSubmit={submitHandler}>
                 <label htmlFor="player">Please enter your name to start the game: </label>
-                <input type="text" id="player" name="player" />
+                <input type="text" id="player" name="player" onChange={playerNameChangeHandler}/>
                 <input type="submit" value="ok"/>
             </form>
         </div>
