9cf3b1a7401f000ee892bc794e628dc2704a4499
Nicola Hearn <nfhearn@gmail.com>
2023-09-30 08:32:33 +0100

    adds console log new game to newPlayerHandler

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React, { useState } from 'react';
-import LetsPlay from './components/LetsPlay';
-import './App.css';
-import GreetUser from './components/GreetUser';
-import DisplayWord from './components/DisplayWord';
-import Result from './components/Result';
-import EndGame from './components/EndGame';
+import React, { useState } from "react";
+import LetsPlay from "./components/LetsPlay";
+import "./App.css";
+import GreetUser from "./components/GreetUser";
+import DisplayWord from "./components/DisplayWord";
+import Result from "./components/Result";
+import EndGame from "./components/EndGame";
 
 function App() {
-
-  const [choice, setChoice] = useState('');
-  const [greetUser, setGreetUser] = useState('')
+  const [choice, setChoice] = useState("");
+  const [greetUser, setGreetUser] = useState("");
+  const [newGame, setNewGame] = useState("");
 
   const cheeseHandler = () => {
-    setChoice('CHEESE');
+    setChoice("CHEESE");
     console.log(choice);
- }
+  };
 
- const ikeaHandler = () => {
-    setChoice('IKEA');
+  const ikeaHandler = () => {
+    setChoice("IKEA");
     console.log(choice);
- } 
+  };
 
-  const newPlayerHandler = (enteredName) => {
-      setGreetUser(<GreetUser name={enteredName} />)
-      console.log(enteredName);
-   }
- 
+  const newPlayerHandler = (enteredName, game) => {
+    setGreetUser(<GreetUser name={enteredName} />);
+    console.log(enteredName + "  " + game);
+  };
 
-   return (
+  return (
     <div className="App">
       <h1 className="app-header">"Ikea or Cheese"</h1>
-      <LetsPlay onSubmitName={newPlayerHandler}/>
+      <LetsPlay onSubmitName={newPlayerHandler} />
       <div>{greetUser}</div>
-      <DisplayWord on/>
+      <div>{newGame}</div>
+      <DisplayWord on />
       <p>{choice}</p>
-      <Result >{choice}</Result>
+      <Result>{choice}</Result>
       <button onClick={ikeaHandler}>IKEA</button>
       <button onClick={cheeseHandler}>CHEESE</button>
       <EndGame />
--- src/components/LetsPlay.js
+++ src/components/LetsPlay.js
@@ -1,35 +1,42 @@
-import React, { useState } from 'react';
-import GreetUser from './GreetUser';
+import React, { useState } from "react";
+import GreetUser from "./GreetUser";
 function LetsPlay(props) {
-   
-    const [enteredName, setEnteredName] = useState('');
-    
+  const [enteredName, setEnteredName] = useState("");
 
-    const playerNameChangeHandler = (event) => {
-        setEnteredName(event.target.value)
-    };
+  const playerNameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
 
-    let player;
+  let player;
+  let game = "new Game";
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        player = enteredName;
-        
-        props.onSubmitName(player)
-        setEnteredName('');
-    }
+  const submitHandler = (event) => {
+    event.preventDefault();
+    player = enteredName;
 
-    return(
-        <div data-testid="startGame">
-            <h2>Let's play Ikea or Cheese</h2>
-            <form onSubmit={submitHandler}>
-                <label htmlFor="player">Please enter your name to start the game: </label>
-                <input type="text" id="player" value={enteredName} name="player" onChange={playerNameChangeHandler}/>
-                <input type="submit" value="ok"/>
-            </form>
-            <div></div>
-        </div>
-    )
+    props.onSubmitName(player, game);
+    setEnteredName("");
+  };
+
+  return (
+    <div data-testid="startGame">
+      <h2>Let's play Ikea or Cheese</h2>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="player">
+          Please enter your name to start the game:{" "}
+        </label>
+        <input
+          type="text"
+          id="player"
+          value={enteredName}
+          name="player"
+          onChange={playerNameChangeHandler}
+        />
+        <input type="submit" value="ok" />
+      </form>
+      <div></div>
+    </div>
+  );
 }
 
-export default LetsPlay
+export default LetsPlay;
