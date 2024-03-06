9d73ac21e7eda130a7f8b220fc3f69080a56edfa
Nicola Hearn <nfhearn@gmail.com>
2024-03-06 15:18:15 +0000

    sets up with basic styling

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-it("contains the header 'Ikea or Cheese'", () => {
-  render(<App />);
-  const linkElement = screen.getByRole("heading", {name: "Ikea or Cheese"})
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  it("contains the header 'Ikea or Cheese'", () => {
+    render(<App />);
+    const linkElement = screen.getByRole("heading", { name: "IKEA OR CHEESE" });
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("renders an input box with the correct label", () => {
+    render(<App />);
+
+    const inputElement = screen.getByLabelText(
+      "Let's play ikea or cheese, please enter your name to start the game:"
+    );
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement.tagName).toBe("INPUT");
+  });
 });
-
-
--- src/components/LetsPlay.js
+++ src/components/LetsPlay.js
@@ -1,35 +1,44 @@
-import React, { useState } from 'react';
-import GreetUser from './GreetUser';
+import React, { useState } from "react";
+import GreetUser from "./GreetUser";
 function LetsPlay(props) {
-   
-    const [enteredName, setEnteredName] = useState('');
-    
+  const [enteredName, setEnteredName] = useState("");
 
-    const playerNameChangeHandler = (event) => {
-        setEnteredName(event.target.value)
-    };
+  const playerNameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
 
-    let player;
+  let player;
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        player = enteredName;
-        
-        props.onSubmitName(player)
-        setEnteredName('');
-    }
+  const submitHandler = (event) => {
+    event.preventDefault();
+    player = enteredName;
 
-    return(
-        <div data-testid="startGame">
-            <h2>Let's play Ikea or Cheese</h2>
-            <form onSubmit={submitHandler}>
-                <label htmlFor="player">Please enter your name to start the game: </label>
-                <input type="text" id="player" value={enteredName} name="player" onChange={playerNameChangeHandler}/>
-                <input type="submit" value="ok"/>
-            </form>
-            <div></div>
-        </div>
-    )
+    props.onSubmitName(player);
+    setEnteredName("");
+  };
+
+  return (
+    <div data-testid="startGame" className="mt-4">
+      <form onSubmit={submitHandler}>
+        <label className="my-2" htmlFor="player">
+          Let's play ikea or cheese, please enter your name to start the game:{" "}
+        </label>
+        <input
+          className="block w-full rounded-md border-0 px-3.5 py-2 my-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+          type="text"
+          id="player"
+          value={enteredName}
+          name="player"
+          onChange={playerNameChangeHandler}
+        />
+        <input
+          type="submit"
+          value="ok"
+          className="block w-full rounded-md bg-blue-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+        />
+      </form>
+    </div>
+  );
 }
 
-export default LetsPlay
+export default LetsPlay;
