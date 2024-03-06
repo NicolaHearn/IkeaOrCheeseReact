13edfacafcd11f7a6deeebba7786247e3effba70
Nicola Hearn <nfhearn@gmail.com>
2024-03-06 10:23:57 +0000

    test passing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React, { useState } from 'react';
-import LetsPlay from './components/LetsPlay';
-import './App.css';
-import GreetUser from './components/GreetUser';
-import DisplayWord from './components/DisplayWord';
-import Result from './components/Result';
-import EndGame from './components/EndGame';
+import React, { useState } from "react";
+import LetsPlay from "./components/LetsPlay";
+import "./App.css";
+import GreetUser from "./components/GreetUser";
+import DisplayWord from "./components/DisplayWord";
+import Result from "./components/Result";
+import EndGame from "./components/EndGame";
 
 function App() {
-
-  const [choice, setChoice] = useState('');
-  const [greetUser, setGreetUser] = useState('')
+  const [choice, setChoice] = useState("");
+  const [greetUser, setGreetUser] = useState("");
 
   const cheeseHandler = () => {
-    setChoice('CHEESE');
+    setChoice("CHEESE");
     console.log(choice);
- }
+  };
 
- const ikeaHandler = () => {
-    setChoice('IKEA');
+  const ikeaHandler = () => {
+    setChoice("IKEA");
     console.log(choice);
- } 
+  };
 
   const newPlayerHandler = (enteredName) => {
-      setGreetUser(<GreetUser name={enteredName} />)
-      console.log(enteredName);
-   }
- 
+    setGreetUser(<GreetUser name={enteredName} />);
+    console.log(enteredName);
+  };
 
-   return (
+  return (
     <div className="App">
-      <h1 className="app-header">"Ikea or Cheese"</h1>
-      <LetsPlay onSubmitName={newPlayerHandler}/>
+      <h1 className="app-header">Ikea or Cheese</h1>
+      <LetsPlay onSubmitName={newPlayerHandler} />
       <div>{greetUser}</div>
-      <DisplayWord on/>
+      <DisplayWord on />
       <p>{choice}</p>
-      <Result >{choice}</Result>
+      <Result>{choice}</Result>
       <button onClick={ikeaHandler}>IKEA</button>
       <button onClick={cheeseHandler}>CHEESE</button>
       <EndGame />
