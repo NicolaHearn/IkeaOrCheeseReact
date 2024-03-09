6658fa0a6f9e061359335bd47448c6d092c7f439
Nicola Hearn <nfhearn@gmail.com>
2024-03-09 14:01:26 +0000

    updates round file

--- src/gameLogic/index.js
+++ src/gameLogic/index.js
@@ -1,14 +1,14 @@
 const Game = require("./game");
-const Round = require("./round");
+// const Round = require("./round");
 const prompt = require("prompt-sync")();
 
 let sessionInPlay = "y";
 while (sessionInPlay === "y") {
   console.log("Let's play Ikea or Cheese");
   let userName = prompt("Please enter your name to start the game: ");
-  console.log(`Hello ${userName}, type "i" for ikea or "c" for cheese`)
+  console.log(`Hello ${userName}, type "i" for ikea or "c" for cheese`);
   let game = new Game(userName);
-  let round = new Round();
+  // let round = new Round();
 
   while (game.gameInPlay()) {
     console.log(game.startRound());
@@ -17,11 +17,11 @@ while (sessionInPlay === "y") {
     while (game.checkUserInput(userGuess) !== true) {
       console.log(game.checkUserInput(userGuess));
       console.log(game.wordThisRound);
-      userGuess = prompt("Is it ikea or cheese?")
+      userGuess = prompt("Is it ikea or cheese?");
     }
 
     console.log(game.endRound(userGuess));
-    }
+  }
 
   console.log(game.displayScore());
 
